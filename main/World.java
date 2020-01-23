f49692a3124efb725c6aefe39cd7c7c2d4963290
michlampert <57098578+michlampert@users.noreply.github.com>
2020-01-23 19:05:46 +0100

    Update World.java

--- main/World.java
+++ main/World.java
@@ -5,33 +5,38 @@ import java.util.concurrent.TimeUnit;
 public class World {
 
     public static void main(String[] args) throws InterruptedException {
+        SettingsWindow settingsWindow = new SettingsWindow();
 
-        TorusMap map = new TorusMap(new Vector2d(10, 10));
+        ArrayList<Integer> rulesForAlive = new ArrayList<>();
+        ArrayList<Integer> rulesForNotAlive = new ArrayList<>();
+        int w = 1, h = 1;
+        boolean modeFlag;
 
-//        for(int i = 0; i<=10; i++){
-//            map.addCell(new Vector2d(5 ,i));
-//        }
-//        map.addCell(new Vector2d(1,1));
-//        map.addCell(new Vector2d(1,2));
-//        map.addCell(new Vector2d(2,1));
-//        map.addCell(new Vector2d(2,2));
-
-//        map.addCell(new Vector2d(2,1));
-//        map.addCell(new Vector2d(2,2));
-//        map.addCell(new Vector2d(2,3));
-
-        map.addCell(new Vector2d(0,9));
-        map.addCell(new Vector2d(1,8));
-        map.addCell(new Vector2d(2,8));
-        map.addCell(new Vector2d(2,9));
-        map.addCell(new Vector2d(2,10));
-
-        System.out.print(map);
-        while(!map.cells.isEmpty()){
-            map.nextGeneration();
-            TimeUnit.MILLISECONDS.sleep(1000);
-            System.out.print(map);
-            //map.toProcess.clear();
+        while(true){
+            TimeUnit.MILLISECONDS.sleep(1);
+            if(settingsWindow.flag == true) {
+                for(int i = 0; i < 8; i++){
+                    if(settingsWindow.rulesAliveArray.get(i).isSelected()) rulesForAlive.add(i);
+                    if(settingsWindow.rulesNotAliveArray.get(i).isSelected()) rulesForNotAlive.add(i);
+                }
+                w = Integer.valueOf(settingsWindow.width.getText()) - 1;
+                h = Integer.valueOf(settingsWindow.height.getText()) - 1;
+                modeFlag = settingsWindow.torusMode.isSelected();
+                settingsWindow.dispose();
+                break;
+            }
         }
+
+        TorusMap torusMap = new TorusMap(new Vector2d(w, h), rulesForAlive, rulesForNotAlive);
+        BoundedMap boundedMap = new BoundedMap(new Vector2d(w, h), rulesForAlive, rulesForNotAlive);
+        if(modeFlag){
+            SimulationWindow view = new SimulationWindow(torusMap);
+            view.run();
+        }
+        else{
+            SimulationWindow view = new SimulationWindow(boundedMap);
+            view.run();
+        }
+
     }
 }
